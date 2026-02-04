67457d433e39dfe2269d29c480af024f52b5c18a
erikrt <67928254+erikrt@users.noreply.github.com>
2026-02-04 12:00:15 -0800

    Update index.php

--- web/mobro/index.php
+++ web/mobro/index.php
@@ -118,9 +118,9 @@ $network_wpa = getOrDefault($props['network_wpa'], '');
 $network_hidden = getOrDefault($props['network_hidden'], '0');
 
 // display
-//$display_driver = getOrDefault($props['display_driver'], 'default');
+$display_driver = getOrDefault($props['display_driver'], 'default');
 //$display_hdmi_mode = getOrDefault($props['display_hdmi_mode'], '');
-//$display_rotation = getOrDefault($props['display_rotation'], '0');
+$display_rotation = getOrDefault($props['display_rotation'], '0');
 $display_screensaver = getOrDefault($props['display_screensaver'], 'disabled');
 $display_screensaver_url = getOrDefault($props['display_screensaver_url'], '');
 $display_delay = getOrDefault($props['display_delay'], '5');
